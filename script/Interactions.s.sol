32711325dcc2bb821ee10e5759ec3b097a24edab
SquilliamX <smartchainwizz@gmail.com>
2023-08-26 16:20:52 -0400

    updated :)

--- script/Interactions.s.sol
+++ script/Interactions.s.sol
@@ -9,8 +9,8 @@ import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
 import {BasicNft} from "../src/BasicNft.sol";
 
 contract MintBasicNft is Script {
-    string public constant PUG =
-        "https://ipfs.io/ipfs/QmcJRiVTDEsFPWdUoMWMUr8R9VaqqWd64tWJahJh49JfZy?filename=AI-puppy-nft%20work.png";
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
     function run() external {
         address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
@@ -22,7 +22,9 @@ contract MintBasicNft is Script {
 
     function mintNftOnContract(address contractAddress) public {
         vm.startBroadcast();
-        BasicNft(contractAddress).mintNft(PUG);
+        BasicNft(contractAddress).mintNft(PUG_URI);
         vm.stopBroadcast();
     }
 }
+//   "https://ipfs.io/ipfs/QmcJRiVTDEsFPWdUoMWMUr8R9VaqqWd64tWJahJh49JfZy?filename=AI-puppy-nft%20work.png";
+// https://ipfs.io/ipfs/QmcJRiVTDEsFPWdUoMWMUr8R9VaqqWd64tWJahJh49JfZy
