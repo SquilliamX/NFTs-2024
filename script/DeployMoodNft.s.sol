f08062ad4441b0fe6b2ad968105542dee8d678a2
SquilliamX <smartchainwizz@gmail.com>
2024-12-05 22:41:07 -0500

    Created Scripts to encode Image URIs and deploy the main NFT contracts

---
+++ script/DeployMoodNft.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+
+// Declares the Solidity version to be used
+pragma solidity 0.8.19;
+
+// Import the Forge scripting utilities and our NFT contract
+import {Script, console} from "forge-std/Script.sol";
+import {MoodNft} from "../src/MoodNFT.sol";
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
+
+contract DeployMoodNft is Script {
+    function run() external returns (MoodNft) {
+        string memory sadSvg = vm.readFile("./img/sad.svg");
+        string memory happySvg = vm.readFile("./img/happy.svg");
+
+        vm.startBroadcast();
+        MoodNft moodNft = new MoodNft(svgToImageURI(sadSvg), svgToImageURI(happySvg));
+        vm.stopBroadcast();
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        string memory baseUrl = "data:image/svg+xml;base64,";
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(baseUrl, svgBase64Encoded));
+    }
+}
